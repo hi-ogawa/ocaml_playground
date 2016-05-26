3dc3535254921b4b1a9f76c389cd5b22a2a0417e
hiogawa <hi.ogawa.zz@gmail.com>
2016-05-26 20:48:01 +0900

    add whatever.ml

---
+++ whatever.ml
@@ -0,0 +1,23 @@
+let rec foldl : 'b -> ('b -> 'a -> 'b) -> 'a list -> 'b =
+  fun b f -> function
+          | [] -> b
+          | hd :: tl -> foldl (f b hd) f tl
+;;
+
+let rec foldr : 'b -> ('a -> 'b -> 'b) -> 'a list -> 'b =
+  fun b f -> function
+          | [] -> b
+          | hd :: tl -> foldr b f tl |> f hd
+;;
+
+(* tails recursion *)
+let rec foldr_tail : 'b -> ('a -> 'b -> 'b) -> 'a list -> 'b =
+  fun b f ls ->
+  let rec g : ('b -> 'b) -> 'a list -> 'b =
+    fun k ls ->
+    match ls with
+    | [] -> k b
+    | hd :: tl -> g (fun b -> f hd (k b)) tl
+  in
+  g (fun b -> b) ls
+;;
